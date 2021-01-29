696c1386f982b47bb51b37fef174e96d528a8f4d
hmiram <marek.hmira@avx.com>
2021-01-29 14:34:21 +0100

    update

--- __update.sql
+++ __update.sql
@@ -1,4 +1,4 @@
 ALTER TABLE  `norma_artikl` ADD  `ok_ks_hod_10` INT UNSIGNED NULL ;
 ALTER TABLE  `normy` ADD  `ks_hod_10` double UNSIGNED NOT NULL DEFAULT  '0';
 ALTER TABLE  `log_normy` ADD  `ks_hod_10` double UNSIGNED NOT NULL DEFAULT  '0';
-ALTER TABLE  `zaznamy` CHANGE  `kontrola`  `kontrola` ENUM(  '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9',  '10' ) CHARACTER SET latin2 COLLATE latin2_general_ci NULL DEFAULT NULL
+ALTER TABLE  `zaznamy` CHANGE  `kontrola`  `kontrola` ENUM(  '1',  '2',  '3' ) CHARACTER SET latin2 COLLATE latin2_general_ci NULL DEFAULT NULL
